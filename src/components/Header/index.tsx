e7e6372e2f5c4981cd5c132dd2388247754711b6
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-08-06 19:04:18 -0300

    :package: added Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Button, Grid, GridItem, Image } from "@chakra-ui/react";
+
+type HeaderProps = {
+  backButton?: boolean
+}
+
+export function Header({ backButton }: HeaderProps) {
+  return (
+    <Grid templateColumns="repeat(3, 1fr)" maxW="1160px" h="100px" py="7" mx="auto">
+      <GridItem>
+        { backButton && (
+          <Button variant="unstyled">
+            <Image src="./images/back-button.svg" alt="Botão voltar" />
+          </Button>
+        ) }
+      </GridItem>
+      <Image src="./images/logo.svg" alt="Logo" h="full" mx="auto" />
+    </Grid>
+  )
+}
